7b2d634781427728c6ed87695aa82cf975516d22
Julio Fernandez <jfernandez74@gmail.com>
2020-10-25 11:42:19 +0100

    Add user repository

--- facecheck-api/src/main/groovy/com/grananda/repositories/UserRepository.groovy
+++ facecheck-api/src/main/groovy/com/grananda/repositories/UserRepository.groovy
@@ -1,7 +1,14 @@
 package com.grananda.repositories
 
 import com.grananda.domain.User
+import groovy.transform.CompileStatic
+import io.micronaut.context.annotation.Executable
+import io.micronaut.data.annotation.Repository
 import io.micronaut.data.repository.CrudRepository
 
-interface UserRepository extends CrudRepository<User, UUID> {
+@Repository
+@CompileStatic
+interface UserRepository extends CrudRepository<User, String> {
+    @Executable
+    User findByUsernameOrEmail(String username, String email)
 }
