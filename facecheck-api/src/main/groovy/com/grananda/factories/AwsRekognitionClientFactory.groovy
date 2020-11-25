4c92b4e9cd27b29b2dff5beb1de8eb2bb645d661
Julio Fernandez <jfernandez74@gmail.com>
2020-11-25 10:02:12 +0100

    Move AWS credentials to env

--- facecheck-api/src/main/groovy/com/grananda/factories/AwsRekognitionClientFactory.groovy
+++ facecheck-api/src/main/groovy/com/grananda/factories/AwsRekognitionClientFactory.groovy
@@ -1,26 +1,29 @@
 package com.grananda.factories
 
-import io.micronaut.context.annotation.Factory
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
-import software.amazon.awssdk.services.rekognition.RekognitionClient
-
 import javax.inject.Singleton
+
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Value
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.services.rekognition.RekognitionClient
 
 @Factory
 class AwsRekognitionClientFactory {
 
+    @Value('${aws.aws_access_key_id}')
+    String accessKey
+
+    @Value('${aws.aws_secret_access_key}')
+    String secret
+
     @Singleton
-    RekognitionClient rekognitionClient(Optional<String> profile) {
+    RekognitionClient rekognitionClient() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secret)
 
-        return RekognitionClient.builder()
-                .credentialsProvider(generateProfileCredentialsProvider(profile))
-                .build();
-    }
-
-    private ProfileCredentialsProvider generateProfileCredentialsProvider(Optional<String> profile) {
-
-        return ProfileCredentialsProvider.builder()
-                .profileName(profile.orElse("default"))
+        return RekognitionClient
+                .builder()
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
     }
 }
