3d0fbd7717f5f9a6dcaa7fac1410a440e47346ec
Julio Fernandez <jfernandez74@gmail.com>
2020-11-26 07:17:59 +0100

    Add region to AWS instance

--- facecheck-api/src/main/groovy/com/grananda/factories/AwsRekognitionClientFactory.groovy
+++ facecheck-api/src/main/groovy/com/grananda/factories/AwsRekognitionClientFactory.groovy
@@ -6,6 +6,7 @@ import io.micronaut.context.annotation.Factory
 import io.micronaut.context.annotation.Value
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.rekognition.RekognitionClient
 
 @Factory
@@ -17,13 +18,18 @@ class AwsRekognitionClientFactory {
     @Value('${aws.aws_secret_access_key}')
     String secret
 
+    @Value('${aws.aws_region}')
+    String region
+
     @Singleton
     RekognitionClient rekognitionClient() {
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secret)
+        Region region = Region.of(region)
 
         return RekognitionClient
                 .builder()
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(region)
                 .build();
     }
 }
